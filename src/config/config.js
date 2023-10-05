6339c2c604ea54d24f8d62b2e0de7c0d70cdf75a
krunalghaskata <krunalghaskata1223@gmail.com>
2023-10-05 22:22:46 +0530

    config add

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const config = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT ||7600,
   DB: {
     CONNECTION_URL: process.env.DB_CONNECTION_URL,
   },
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const CONFIG = require("../src/config/config");
 const UserRouter = require("./Router/user/Router");
 const blogRouter = require("./Router/blog/blogRoute");
 
@@ -13,7 +14,7 @@ app.use("/api/user", UserRouter.Router);
 app.use("/api/blog", blogRouter.Router);
 
 mongoose
-  .connect("mongodb://localhost:27017/socialMedia")
+  .connect(CONFIG.DB.CONNECTION_URL)
   .then(() => {
     console.log("dataBase connection successfully");
   })
@@ -21,6 +22,6 @@ mongoose
     console.log("connection failed", error);
   });
 
-app.listen(2500, () => {
-  console.log("server listening port 2500");
+app.listen(CONFIG.PORT, () => {
+  console.log(`server start on port http://localhost:${CONFIG.PORT}`);
 });
